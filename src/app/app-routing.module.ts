9cbbfa0f173f430b57b9e3a3a64857b6d3ca09db
Dan Patton <pattondh@appstate.edu>
2019-04-24 16:14:43 -0400

    v1 homepage will be adding reviews section soon

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { InventoryComponent } from './inventory/inventory.component';
-
+import { HomepageComponent } from './homepage/homepage.component';
 const routes: Routes = [
-  { path: '', redirectTo: '/homePage', pathMatch: 'full'},
+  { path: '', redirectTo: 'app-homepage', pathMatch: 'full'},
   { path: 'admin', component: AdminComponent },
   { path: 'app-contact-us', component: ContactUsComponent },
-  { path: 'app-inventory', component: InventoryComponent }
+  { path: 'app-inventory', component: InventoryComponent },
+  { path: 'app-homepage', component: HomepageComponent }
 ];
 
 @NgModule({
